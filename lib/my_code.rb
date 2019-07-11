a1e85acfd33ae0f53492bd29c8b1f7b56daea372
Matthew Listor <mattlistor@gmail.com>
2019-07-11 16:57:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,10 +30,11 @@ def reduce_to_total(source_array, starting_point = 0)
   return starting_point
 end
 
-def reduce_to_all_true(source_array) 
+def reduce_to_all_true(source_array)
   source_array.length.times do |i|
     if source_array[i] = false 
       return false
+    end
   end
   return true
 end
